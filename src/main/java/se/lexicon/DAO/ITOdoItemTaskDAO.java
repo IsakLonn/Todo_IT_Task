253133d510ff1f08fec9a73abc0645878e2e2366
IsakLönn <illidan1996@hotmail.com>
2023-12-19 15:51:55 +0100

    Implemented Todo Item Task DAO

---
+++ src/main/java/se/lexicon/DAO/ITOdoItemTaskDAO.java
@@ -0,0 +1,15 @@
+package se.lexicon.DAO;
+
+import se.lexicon.TodoItemTask;
+
+import java.util.Collection;
+
+public interface ITOdoItemTaskDAO {
+
+    public TodoItemTask persist(TodoItemTask todoItemTask);
+    public TodoItemTask findById(int id);
+    public Collection<TodoItemTask> findAll();
+    public Collection<TodoItemTask> findByAssignedStatus(boolean status);
+    public Collection<TodoItemTask> findByPersonId(int id);
+    public void remove(int id);
+}
---
+++ src/main/java/se/lexicon/DAO/TodoItemTaskDAOCollection.java
@@ -0,0 +1,61 @@
+package se.lexicon.DAO;
+
+import se.lexicon.TodoItem;
+import se.lexicon.TodoItemTask;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+public class TodoItemTaskDAOCollection implements ITOdoItemTaskDAO{
+
+    private ArrayList<TodoItemTask> list = new ArrayList<>();
+
+    @Override
+    public TodoItemTask persist(TodoItemTask todoItemTask) {
+        if(todoItemTask == null) return null;
+        list.add(todoItemTask);
+        return todoItemTask;
+    }
+
+    @Override
+    public TodoItemTask findById(int id) {
+        for (TodoItemTask item: list) {
+            if(item.getId() == id) return item;
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<TodoItemTask> findAll() {
+        return list;
+    }
+
+    @Override
+    public Collection<TodoItemTask> findByAssignedStatus(boolean status) {
+        ArrayList<TodoItemTask> itemsByAssignedStatus = new ArrayList<>();
+        for (TodoItemTask item: list) {
+            if(item.isAssigned() == status) itemsByAssignedStatus.add(item);
+        }
+        return itemsByAssignedStatus;
+    }
+
+    @Override
+    public Collection<TodoItemTask> findByPersonId(int id) {
+        ArrayList<TodoItemTask> itemsByPersonId = new ArrayList<>();
+        for (TodoItemTask item: list) {
+            if(item.getAsignee().getId() == id) itemsByPersonId.add(item);
+        }
+        return itemsByPersonId;
+    }
+
+    @Override
+    public void remove(int id) {
+        TodoItemTask toRemove = null;
+        for (TodoItemTask item: list) {
+            if(Objects.equals(item.getId(), id)) toRemove = item;
+        }
+        if(toRemove != null) list.remove(toRemove);
+    }
+}
